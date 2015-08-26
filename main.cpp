f2a9e5c1a438e09c3979badde9fa605f3dfb186d
eblab <info@cneb.de>
2015-08-26 08:55:17 +0200

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26.08.2015 08:51:23
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  eblabr
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
